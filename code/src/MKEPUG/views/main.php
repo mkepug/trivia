fe91052e5e914b55cb2f22effa517444713afd7b
Chris Smith <chris@cgsmith.net>
2014-05-14 09:17:47 -0500

    the view and question test

---
+++ code/src/MKEPUG/views/main.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<header>Who wants to be a MKEPUGinaire?</header>
+<?= $viewContents->render(); ?>
+<footer>Opensauced MKEPUG</footer>
+</body>
+</html>
